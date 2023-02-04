8a7f54fb81b1a395d13c069f07aeb89ef81bbbe8
Victor Dubeux <adv.victordubeux@gmail.com>
2023-02-04 14:29:59 -0300

    P14 C2 - Envio definitivo de projeto

---
+++ ProjetoQuatorze/ProjetoQuatorze/Program.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoQuatorze;
+using System;
+using System.Globalization;
+
+namespace Exercicio
+{
+    class ProjetoQuatorze
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Informe a altura do retângulo: ");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe a largura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write($"ÁREA: {r.Area().ToString("F2", CultureInfo.InvariantCulture)}.");
+            Console.WriteLine();
+            Console.Write($"PERÍMETRO: {r.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}.");
+            Console.WriteLine();
+            Console.Write($"DIAGONAL: {r.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjetoQuatorze/ProjetoQuatorze/Retangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ProjetoQuatorze
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal(){
+            return Math.Sqrt(Math.Pow(Largura,2) + Math.Pow(Altura,2));
+        }
+    }
+}
